d190ab18f6be3a64bd754332d59441d8addbae87
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-07-02 13:46:37 +0300

    add gateway service + db for gateway (prisma)

--- apps/gateway/src/gateway.service.ts
+++ apps/gateway/src/gateway.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, User } from '@prisma/gateway';
 
 @Injectable()
 export class GatewayService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaClient) { }
+  async getUsers(): Promise<User[]> {
+    const user = await this.prisma.user.findMany();
+    return user;
   }
 }
