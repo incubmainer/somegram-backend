a5a330ec40d59a0561554731fdc728394bd90336
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-07-03 22:19:10 +0300

    add debug global prefix

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,6 +3,7 @@ import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
+  app.setGlobalPrefix('/backend-api');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
