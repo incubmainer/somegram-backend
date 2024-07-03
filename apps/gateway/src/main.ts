eaef0e6a499678c04b8d48c76b4c9bb3761e25a1
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-07-03 15:42:02 +0300

    fix deploy

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -4,6 +4,6 @@ import { GatewayModule } from './gateway.module';
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.APP_PORT || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
