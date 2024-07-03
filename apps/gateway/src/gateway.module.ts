421a4f7b00d2b53b1285feca9952ec32535ddaca
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-07-03 23:23:56 +0300

    some

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,16 +1,11 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { PrismaClient } from '@prisma/gateway';
-import { LoggerMiddleware } from './tmp.middleware';
 
 @Module({
   imports: [],
   controllers: [GatewayController],
   providers: [GatewayService, PrismaClient],
 })
-export class GatewayModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*'); // Использовать для всех маршрутов
-  }
-}
+export class GatewayModule { }
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
-import { NotFoundExceptionFilter } from './tmp.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  app.setGlobalPrefix('/backend-api');
-  app.useGlobalFilters(new NotFoundExceptionFilter());
+  app.setGlobalPrefix('/api/v1');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
