018aab7aead30deed71eadb54d596c3ed761c4a8
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-07-03 21:46:46 +0300

    delete global prefix

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,7 +3,6 @@ import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
