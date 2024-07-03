9e929cf7062db27721fafa3483ae3306a7df19f1
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-07-03 23:09:20 +0300

    test

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { NotFoundExceptionFilter } from './tmp.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   app.setGlobalPrefix('/backend-api');
+  app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
