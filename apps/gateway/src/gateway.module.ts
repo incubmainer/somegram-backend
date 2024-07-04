32e895f820354d1fbc598d4fa9e2ab4fe35006d7
OsirisAnubis <olegshulgakalendar@gmail.com>
2024-07-04 11:54:59 +0300

    add config to gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { PrismaClient } from '@prisma/gateway';
+import { ConfigModule } from '@nestjs/config';
+import { loadEnvFileNames } from './common/config/load-env-file-names';
+import { finalConfig } from './common/config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: false,
+      envFilePath: loadEnvFileNames(),
+      load: [finalConfig],
+    }),
+  ],
   controllers: [GatewayController],
   providers: [GatewayService, PrismaClient],
 })
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { ConfigService } from '@nestjs/config';
+import { AppConfig } from './common/config/configs/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   app.setGlobalPrefix('/api/v1');
-  await app.listen(process.env.PORT || 3000);
+  const configService = app.get(ConfigService);
+  const appConfig = configService.get<AppConfig>('app');
+  await app.listen(appConfig.PORT);
 }
 bootstrap();
