5bc1ace2dc06289f3d4bd895cdcb75565530ea28
Valentin <valentin_00794@bk.ru>
2024-11-07 10:07:07 +0300

    fix: db string for tests, change s3 connection

--- apps/gateway/src/common/config/configs/db.config.ts
+++ apps/gateway/src/common/config/configs/db.config.ts
@@ -2,17 +2,9 @@ export class DbConfig {
   readonly gatewayDatabaseUrl: string;
 }
 
-const pgRegex = /^postgres:\/\/[^:]+:[^@]+@[^:]+:\d+\/.+$/;
-
-function isPostgresConnectionString(connectionString: string) {
-  return pgRegex.test(connectionString);
-}
-
 export const dbConfig = (): DbConfig => {
   const databaseUrl = process.env.GATEWAY_DATABASE_URL;
   if (!databaseUrl) throw new Error('GATEWAY_DATABASE_URL is not set');
-  if (!isPostgresConnectionString(databaseUrl))
-    throw new Error(`Invalid database URL: ${databaseUrl}`);
   return {
     gatewayDatabaseUrl: databaseUrl,
   };
