8fd366fea1da1ef966e0302a878e42c4c698cd2c
Thedd <mixailmar4uk78@gmail.com>
2025-02-26 14:58:10 +0200

    fix: remove not use library and clear aliases, update env patch into file microservice

--- apps/files/src/common/config/load-env-file-names.ts
+++ apps/files/src/common/config/load-env-file-names.ts
@@ -1,5 +1,4 @@
 export const loadEnvFileNames = (): string[] => {
-  if (process.env.NODE_ENV === 'development')
-    return ['./apps/files/.env.dev.dev.dev.dev'];
+  if (process.env.NODE_ENV === 'development') return ['./apps/files/.env.dev'];
   return [];
 };
--- libs/requests/src/requests.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { Inject } from '@nestjs/common';
-import { RequestsServiceToken } from './requests.constants';
-
-export function InjectRequestsService() {
-  return Inject(RequestsServiceToken);
-}
